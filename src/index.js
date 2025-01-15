5a18e3a5f2b52102ee52cec63a5de5e80d12a17d
Juan Cristobal <65052633+juancristobalgd1@users.noreply.github.com>
2025-01-15 03:39:13 +0100

    update file

--- src/index.js
+++ src/index.js
@@ -1,15 +1,13 @@
-export { component } from "./component.js";
-export { html } from "./templates/engine.js";
-export { useSignal, useBatch } from "./hooks/signals.js";
-export { useRef } from "./hooks/ref.js";
-export { useComputed } from "./hooks/signals.js";
-export { useMemo } from "./hooks/memo.js";
-export { useEmit } from "./events.js";
-export { useEffect } from "./hooks/effects.js";
-// export { router } from "./router.js";
-
-// Lifecycle hooks
-export {
+import { component } from "./component.js";
+import { html } from "./templates/engine.js";
+import { useSignal, useBatch } from "./hooks/signals.js";
+import { useRef } from "./hooks/ref.js";
+import { useComputed } from "./hooks/signals.js";
+import { useMemo } from "./hooks/memo.js";
+import { useEmit } from "./events.js";
+import { useEffect } from "./hooks/effects.js";
+import { useStore } from "./hooks/store.js";
+import {
     beforeMount,
     onMount,
     beforeUpdate,
@@ -17,3 +15,22 @@ export {
     onDestroy,
     onEffect,
 } from "./lifecycle.js";
+
+export {
+    component,
+    html,
+    useSignal,
+    useBatch,
+    useRef,
+    useComputed,
+    useMemo,
+    useEmit,
+    useEffect,
+    useStore,
+    beforeMount,
+    onMount,
+    beforeUpdate,
+    onUpdate,
+    onDestroy,
+    onEffect
+};
